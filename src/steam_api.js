fcb3a59556e8c6a02e75578b65e515329bdd2eb8
Maho Morichika <nat@magicnat.com>
2019-04-18 14:37:05 -0400

    fix key rotation

--- src/steam_api.js
+++ src/steam_api.js
@@ -1,12 +1,13 @@
 var SteamAPI = function (api_key) {
     if (!Array.isArray(api_key)) api_key = [api_key];
 
-    var api_do = function (key, cat, method, version, params) {
+    var api_do = function (a_key, cat, method, version, params) {
+        console.log('try with key: ', a_key);
         return new Promise((res, rej) => {
             var query = '';
-            for (key in params) query += `&${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`;
+            for (var key in params) query += `&${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`;
             var xhr = new XMLHttpRequest();
-            xhr.open('GET', `http://steam.kra.moe/${cat}/${method}/${version}?key=${api_key}&format=json${query}`);
+            xhr.open('GET', `http://steam.kra.moe/${cat}/${method}/${version}?key=${a_key}&format=json${query}`);
             xhr.setRequestHeader('Accept-Language','en-US');
             xhr.send();
             xhr.onload = function () {
