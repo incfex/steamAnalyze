91e26ec414565df3711378363764f3cabf3a9960
stan <zchen24@syr.edu>
2019-05-08 13:33:13 -0400

    add comment for game_time and friends_commend

--- src/get_owned_games.js
+++ src/get_owned_games.js
@@ -1,3 +1,4 @@
+//give api wrapper needed arguments for getting user profile
 var getProfile = async (steam_api, steam_id) => {
     return steam_api(
         "IPlayerService", "GetOwnedGames", "v0001", {steamid: steam_id,include_appinfo : 1, include_played_free_games : 1 }
